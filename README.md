d7939285ccf1bce62b159b62b7b8526c31630884
“Talexandra03” <Tatatianacgrafip@gmail.com>
2018-10-17 14:30:43 -0500

    angular completo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Articulos
+# Articulos183
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ArticulosComponent } from './articulos/articulos.component';
+import { CrearArticuloComponent } from './crear-articulo/crear-articulo.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
+import { MostrarArticuloComponent } from './mostrar-articulo/mostrar-articulo.component';
+
+import {ArticulosService} from './articulos.service';
+import {UsuariosService} from './usuarios.service';
+
 
 const rutas:Routes=[
-  {path:'',component:InicioComponent, pathMatch:'full'},//ruta vacia
-{path:'inicio', component:InicioComponent},
-{path:'articulos', component:ArticulosComponent},
-{path: '**', redirectTo: '/', pathMatch: 'full'}//Ruta que no existe
+  {path: '', component: InicioComponent, pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'articulos', component: ArticulosComponent},
+  {path: 'iniciar_sesion', component: IniciarSesionComponent},
+  {path: '**', redirectTo: '/', pathMatch: 'full'}
 ];
 
 @NgModule({
@@ -19,13 +30,22 @@ const rutas:Routes=[
     AppComponent,
     BarraNavegacionComponent,
     InicioComponent,
-    ArticulosComponent
+    ArticulosComponent,
+    CrearArticuloComponent,
+    IniciarSesionComponent,
+    CrearCuentaComponent,
+    MostrarArticuloComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(rutas)
+    RouterModule.forRoot(rutas),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ArticulosService,
+    UsuariosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
